88e111d9c95ee603307568e7db896d8b1965fe92
george-kirby <georgeskirby@btinternet.com>
2019-11-16 20:15:23 +0000

    corrected reposEndpoint

--- src/adapters/API.js
+++ src/adapters/API.js
@@ -1,13 +1,13 @@
 const githubEndpoint = "https://api.github.com/"
 const usersEndpoint = `${githubEndpoint}users/`
 
-const reposEndpoint = username => `${usersEndpoint}${username}/repos/`
+const reposEndpoint = username => `${usersEndpoint}${username}/repos`
 
 const getUserRepos = username => {
     console.log(reposEndpoint(username))
     // return fetch(reposEndpoint(username), {
     //     headers: {
-    //         Authorization: 
+    //         Authorization: `token TOKEN_GOES_HERE`
     //     }
     // })
     return fetch(reposEndpoint(username))
