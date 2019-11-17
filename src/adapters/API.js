539b33e200340e93d804ad0e6ac8be0569bed896
george-kirby <georgeskirby@btinternet.com>
2019-11-17 08:17:07 +0000

    paginates 100 per page

--- src/adapters/API.js
+++ src/adapters/API.js
@@ -1,16 +1,15 @@
 const githubEndpoint = "https://api.github.com/"
 const usersEndpoint = `${githubEndpoint}users/`
 
-const reposEndpoint = username => `${usersEndpoint}${username}/repos`
+const reposEndpoint100PerPage = username => `${usersEndpoint}${username}/repos?per_page=100`
 
 const getUserRepos = username => {
-    console.log(reposEndpoint(username))
     // return fetch(reposEndpoint(username), {
     //     headers: {
     //         Authorization: `token TOKEN_GOES_HERE`
     //     }
     // })
-    return fetch(reposEndpoint(username))
+    return fetch(reposEndpoint100PerPage(username))
     .then(resp => resp.json())
 }
 
